529030deb68fdbf27f57cf349231bf2ab3f173e3
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-10 14:29:18 +0100

    adding swagger

--- public/css/swagger-initializer.js
+++ public/css/swagger-initializer.js
@@ -3,7 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://swaggerr-api-ui.herokuapp.com/swagger-api.json",
+    url:"https://event-reservation-system.herokuapp.com/storage/files/swagger-api.json",
+    // url: "https://swaggerr-api-ui.herokuapp.com/swagger-api.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
