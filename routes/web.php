971ac9a1d136474ccf761464b78b0e36c3325851
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-12 10:58:20 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
