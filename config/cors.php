de0b4f13ace2f092a9b7f56dc55a4c956117819f
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-15 19:33:49 +0100

    adding csrf to blade template

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' =>  ["Content-Type", "Authorization"],
+    'allowed_headers' =>  ["Content-Type", "Authorization"], # ["*"]
 
     'exposed_headers' => [],
 
