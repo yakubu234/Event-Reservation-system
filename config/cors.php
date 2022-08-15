91dfdc3d9be33dccd6d88abe3e24a171685e2b63
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-15 17:03:40 +0100

    solving cors origin on heroku

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://event-reservation-system.herokuapp.com', 'http://localhost:8080', 'http://localhost:8000'],
 
     'allowed_origins_patterns' => [],
 
