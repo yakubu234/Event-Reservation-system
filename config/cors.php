39f1db25da5fd256613c939a49ca249f2e3853b3
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-16 17:39:04 +0100

    remodifying the cors.php file

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://event-reservation-system.herokuapp.com', 'http://localhost:8080', 'http://localhost:8000', 'http://localhost'],
+    'allowed_origins' => ['https://event-reservation-system.herokuapp.com', 'http://localhost:8080', 'http://localhost:8000', 'http://localhost', '*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' =>  ["Content-Type", "Authorization"], # ["*"]
+    'allowed_headers' =>  ['*'], # ["*"]
 
     'exposed_headers' => [],
 
