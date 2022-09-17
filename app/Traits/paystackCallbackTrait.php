c8419bad61e606fa3fd69e1625796225d48fa8a4
yakubu234 <yakubuabiola2003@gmail.com>
2022-09-17 18:07:08 +0100

    commenting the paystack callback trait

--- app/Traits/paystackCallbackTrait.php
+++ app/Traits/paystackCallbackTrait.php
@@ -8,28 +8,28 @@ trait paystackCallbackTrait
 {
     use ResponseTrait;
 
-    public function getCallbackData($reference)
-    {
-        return $this->validate($reference);
-    }
+    // public function getCallbackData($reference)
+    // {
+    //     return $this->validate($reference);
+    // }
 
-    public function validate($reference)
-    {
-        $this->paystack_url = (config('services.paystack.base_url'));
-        $this->paystack_key = (config('services.paystack.secret'));
-        try {
+    // public function validate($reference)
+    // {
+    //     $this->paystack_url = (config('services.paystack.base_url'));
+    //     $this->paystack_key = (config('services.paystack.secret'));
+    //     try {
 
-            $response = Http::withToken($this->paystack_key)->asForm()->get(
-                "{$this->paystack_url}transaction/verify/" . rawurlencode($reference)
-            )->json();
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
+    //         $response = Http::withToken($this->paystack_key)->asForm()->get(
+    //             "{$this->paystack_url}transaction/verify/" . rawurlencode($reference)
+    //         )->json();
+    //     } catch (\Throwable $th) {
+    //         return $th->getMessage();
+    //     }
 
-        return $response;
-        if ($response['status'] = 'true') return $this->success($response['data']['authorization_url'], 'Success, Redirect to payment page', 200);
-        #if ($response['status'] = 'true') return  redirect($response['data']['authorization_url']);
+    //     return $response;
+    //     if ($response['status'] = 'true') return $this->success($response['data']['authorization_url'], 'Success, Redirect to payment page', 200);
+    //     #if ($response['status'] = 'true') return  redirect($response['data']['authorization_url']);
 
-        return $this->error('API returned error: ' . $response['message'], 402);
-    }
+    //     return $this->error('API returned error: ' . $response['message'], 402);
+    // }
 }
